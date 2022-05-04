218591b3d3aa389bcdbfc26b2d0b2a28b39fb9c4
argon1025 <argon1025@gmail.com>
2022-05-04 17:12:37 +0900

    refactor: 어드민 토큰 데코레이터명 변경

--- library/passport/decorator/admin-token.decorator.ts
+++ library/passport/decorator/admin-token.decorator.ts
@@ -2,7 +2,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
 import { AdminJwtTokenPayload } from 'library/jwt/type/admin-jwt-token-payload.type';
 
-export const JwtRefreshTokenData = createParamDecorator(
+export const AdminTokenData = createParamDecorator(
   (_: unknown, ctx: ExecutionContext): AdminJwtTokenPayload => {
     const request = ctx.switchToHttp().getRequest();
     // NOTE: passport 검증후에 사용되는 데코레이터로 반드시 AdminJwtTokenPayload 타입을 반환합니다
