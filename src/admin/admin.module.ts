94993ff845b8ab1006d6fd092a7a962da2e0cf0e
argon1025 <argon1025@gmail.com>
2022-05-04 16:31:42 +0900

    feat: Module DI

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,14 +2,23 @@ import { Module } from '@nestjs/common';
 
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { CookieModule } from 'library/cookie/cookie.module';
 import { CryptoModule } from 'library/crypto/crypto.module';
+import { TokenModule } from 'library/jwt/token.module';
 import { PrismaModule } from 'library/prisma/prisma.module';
 import { TimeModule } from 'library/time/time.module';
 import { UuidModule } from 'library/uuid/uuid.module';
 import { AdminRepository } from 'src/admin/admin.repository';
 
 @Module({
-  imports: [PrismaModule, TimeModule, UuidModule, CryptoModule],
+  imports: [
+    PrismaModule,
+    TimeModule,
+    UuidModule,
+    CryptoModule,
+    CookieModule,
+    TokenModule,
+  ],
   controllers: [AdminController],
   providers: [AdminService, AdminRepository],
 })
