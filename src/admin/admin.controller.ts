ae8094563fea0106c1bbf4290def6bd19d6b35a3
argon1025 <argon1025@gmail.com>
2022-05-04 16:31:33 +0900

    feat: loginAdmin Contorller 메서드 작성

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,17 +1,41 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Res } from '@nestjs/common';
 
-import { JoinAdmin } from 'src/admin/admin.decorator';
+import { Response } from 'express';
+import { CookieService } from 'library/cookie/cookie.service';
+import { TokenService } from 'library/jwt/token.service';
+import { JoinAdmin, LoginAdmin } from 'src/admin/admin.decorator';
 import { AdminService } from 'src/admin/admin.service';
 
 import { JoinAdminRequestBodyDto } from 'src/admin/dto/join-admin.dto';
+import { LoginAdminRequestBodyDto } from 'src/admin/dto/login-admin.dto';
 
 @Controller('admin')
 export class AdminController {
-  constructor(private readonly adminService: AdminService) {}
+  constructor(
+    private readonly adminService: AdminService,
+    private readonly tokenService: TokenService,
+    private readonly cookieService: CookieService,
+  ) {}
 
   @JoinAdmin()
   async joinAdmin(@Body() joinAdminRequestBodyDto: JoinAdminRequestBodyDto) {
     await this.adminService.join(joinAdminRequestBodyDto);
+    return null;
+  }
+
+  @LoginAdmin()
+  async loginAdmin(
+    @Body() loginAdminRequestBodyDto: LoginAdminRequestBodyDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const result = await this.adminService.validateAdmin(
+      loginAdminRequestBodyDto,
+    );
+
+    const token = this.tokenService.generateAdminToken({ id: result.id });
+    // NOTE: AdminTokenCookiePayload 인터페이스 준수
+    this.cookieService.setAdminTokenCookie({ response, token });
+
     return null;
   }
 }
