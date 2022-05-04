81f1ddefe2019c54eac76a12471c1437a9201f9e
argon1025 <argon1025@gmail.com>
2022-05-04 16:30:55 +0900

    feat: 관리자 로그인을 위한 Decorator, Dto 작성

--- src/admin/admin.decorator.ts
+++ src/admin/admin.decorator.ts
@@ -6,3 +6,12 @@ export const JoinAdmin = () =>
     Post('auth'),
     ApiOkResponse({ description: '관리자 계정을 생성합니다', type: undefined }),
   );
+
+export const LoginAdmin = () =>
+  applyDecorators(
+    Post('auth/login'),
+    ApiOkResponse({
+      description: '관리자 로그인 성공 및 토큰 발급',
+      type: undefined,
+    }),
+  );
