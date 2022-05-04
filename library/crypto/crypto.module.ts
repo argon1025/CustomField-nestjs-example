90d4412410dcf14e8a1c37fc18cff47e3cbe1af8
argon1025 <argon1025@gmail.com>
2022-05-04 15:12:09 +0900

    feat: Crypto Service, Module Export

---
+++ library/crypto/crypto.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+
+import { CryptoService } from 'library/crypto/crypto.service';
+
+@Module({
+  providers: [CryptoService],
+  exports: [CryptoService],
+})
+export class CryptoModule {}
