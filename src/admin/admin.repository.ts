6b7ba202d3ecd9c3b44c70d5304eb8f800e0d901
argon1025 <argon1025@gmail.com>
2022-05-04 15:38:37 +0900

    feat: 관리자 계정생성을위한 Repository 작성

---
+++ src/admin/admin.repository.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { Admin } from '@prisma/client';
+
+import { PrismaClientService } from 'library/prisma/type/prisma.type';
+
+@Injectable()
+export class AdminRepository {
+  create({
+    prismaClientService,
+    id,
+    email,
+    name,
+    password,
+    createdAt,
+  }: {
+    prismaClientService: PrismaClientService;
+    id: Admin['id'];
+    email: Admin['email'];
+    name: Admin['name'];
+    password: Admin['password'];
+    createdAt: Admin['createdAt'];
+  }) {
+    return prismaClientService.admin.create({
+      data: { id, email, name, password, createdAt },
+    });
+  }
+
+  findFirstByEmail({
+    prismaClientService,
+    email,
+  }: {
+    prismaClientService: PrismaClientService;
+    email: Admin['email'];
+  }) {
+    return prismaClientService.admin.findFirst({
+      where: { email, deletedAt: null },
+    });
+  }
+}
