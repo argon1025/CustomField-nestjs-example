72fa573fdc358ed1076709cd738f1a35fd1e07a5
argon1025 <argon1025@gmail.com>
2022-05-06 23:53:05 +0900

    feat: Customer 로그인을 위한 Decorator, Dto

--- src/customer/customer.decorator.ts
+++ src/customer/customer.decorator.ts
@@ -6,3 +6,9 @@ export const CreateCustomer = () =>
     Post('auth'),
     ApiOkResponse({ description: '유저 생성 성공', type: undefined }),
   );
+
+export const LoginCustomer = () =>
+  applyDecorators(
+    Post('auth/login'),
+    ApiOkResponse({ description: '로그인 성공 및 토큰 발급', type: undefined }),
+  );
