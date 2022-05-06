55954e611c15cea09e39d20df633a707b651da9e
argon1025 <argon1025@gmail.com>
2022-05-06 23:53:34 +0900

    feat: Module DI

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -2,7 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { CustomerController } from './customer.controller';
 import { CustomerService } from './customer.service';
+import { CookieModule } from 'library/cookie/cookie.module';
 import { CryptoModule } from 'library/crypto/crypto.module';
+import { TokenModule } from 'library/jwt/token.module';
 import { PrismaModule } from 'library/prisma/prisma.module';
 import { TimeModule } from 'library/time/time.module';
 import { UuidModule } from 'library/uuid/uuid.module';
@@ -12,7 +14,15 @@ import { CustomerRepository } from 'src/customer/customer.repository';
 import { StoreModule } from 'src/store/store.module';
 
 @Module({
-  imports: [PrismaModule, StoreModule, TimeModule, UuidModule, CryptoModule],
+  imports: [
+    PrismaModule,
+    StoreModule,
+    TimeModule,
+    UuidModule,
+    CryptoModule,
+    TokenModule,
+    CookieModule,
+  ],
   providers: [
     CustomerService,
     CustomerRepository,
