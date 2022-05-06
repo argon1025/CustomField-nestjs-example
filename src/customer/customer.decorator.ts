d1579b765de1dc899fb97c61ec9ae21ae9d605f9
argon1025 <argon1025@gmail.com>
2022-05-07 01:58:56 +0900

    feat: Customer 정보 로드를 위한 Decorator, Dto

--- src/customer/customer.decorator.ts
+++ src/customer/customer.decorator.ts
@@ -1,5 +1,9 @@
-import { applyDecorators, Post } from '@nestjs/common';
+import { applyDecorators, Get, Post } from '@nestjs/common';
 import { ApiOkResponse } from '@nestjs/swagger';
+
+import { CustomerTokenGuard } from 'library/passport/guard/customer-token.guard';
+
+import { GetMeCustomerResponseDto } from 'src/customer/dto/get-me-customer.dto';
 
 export const CreateCustomer = () =>
   applyDecorators(
@@ -11,4 +15,14 @@ export const LoginCustomer = () =>
   applyDecorators(
     Post('auth/login'),
     ApiOkResponse({ description: '로그인 성공 및 토큰 발급', type: undefined }),
+  );
+
+export const GetMeCustomer = () =>
+  applyDecorators(
+    Get('get-me'),
+    ApiOkResponse({
+      description: '고객 정보 로드 성공',
+      type: GetMeCustomerResponseDto,
+    }),
+    CustomerTokenGuard(),
   );
