98c53faccffe2cadd1cda8d50cef4b906630ea1f
argon1025 <argon1025@gmail.com>
2022-05-07 01:59:25 +0900

    feat: Customer 정보 로드를 위한 Contoller

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,12 +1,20 @@
 import { Body, Controller, Res } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 
 import { Response } from 'express';
 import { CookieService } from 'library/cookie/cookie.service';
 import { TokenService } from 'library/jwt/token.service';
-import { CreateCustomer, LoginCustomer } from 'src/customer/customer.decorator';
+import { CustomerTokenData } from 'library/passport/decorator/customer-token.decorator';
+import {
+  CreateCustomer,
+  GetMeCustomer,
+  LoginCustomer,
+} from 'src/customer/customer.decorator';
 import { CustomerService } from 'src/customer/customer.service';
 
+import { CustomerJwtTokenPayload } from 'library/jwt/type/customer-jwt-token-payload.type';
 import { CreateCustomerRequestBodyDto } from 'src/customer/dto/create-customer.dto';
+import { GetMeCustomerResponseDto } from 'src/customer/dto/get-me-customer.dto';
 import { LoginCustomerRequestBodyDto } from 'src/customer/dto/login-customer.dto';
 
 @Controller('customer')
@@ -46,5 +54,24 @@ export class CustomerController {
     this.cookieService.setCustomerTokenCookie({ response, token });
 
     return null;
+  }
+
+  @GetMeCustomer()
+  async getMeCustomer(
+    @CustomerTokenData() customerJwtTokenPayload: CustomerJwtTokenPayload,
+  ) {
+    const { id, store, name, email, CustomerCustomFields } =
+      await this.customerService.getMe({ id: customerJwtTokenPayload.id });
+
+    return new GetMeCustomerResponseDto({
+      userId: id,
+      storeId: store,
+      name,
+      email,
+      customField: CustomerCustomFields.map((val) => ({
+        customFieldId: val.customField,
+        content: val.content as Prisma.JsonArray,
+      })),
+    });
   }
 }
