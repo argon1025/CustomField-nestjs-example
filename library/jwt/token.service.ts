2bf4e9a04478db3e35703b4ca99c0c2a05f5c9fa
argon1025 <argon1025@gmail.com>
2022-05-06 23:50:02 +0900

    feat: Customer 토큰 생성 서비스, 타입, 환경변수 구성

--- library/jwt/token.service.ts
+++ library/jwt/token.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 
 import { AdminJwtTokenPayload } from 'library/jwt/type/admin-jwt-token-payload.type';
+import { CustomerJwtTokenPayload } from 'library/jwt/type/customer-jwt-token-payload.type';
 
 @Injectable()
 export class TokenService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
 
   // NOTE: 관리자 토큰
   generateAdminToken(payload: AdminJwtTokenPayload) {
     return this.jwtService.sign(payload);
+  }
+
+  // NOTE: 고객 토큰
+  generateCustomerToken(payload: CustomerJwtTokenPayload) {
+    return this.jwtService.sign(payload, {
+      secret: this.configService.get<string>('JWT_CUSTOMER_SECRET_KEY'),
+      expiresIn: this.configService.get<string>('JWT_CUSTOMER_EXPIRES_IN'),
+    });
   }
 }
