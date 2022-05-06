37a97fa7e65b056920df62edba47986de1674a84
argon1025 <argon1025@gmail.com>
2022-05-06 23:53:28 +0900

    feat: Customer 로그인을 위한 Controller

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,13 +1,21 @@
-import { Body, Controller } from '@nestjs/common';
+import { Body, Controller, Res } from '@nestjs/common';
 
-import { CreateCustomer } from 'src/customer/customer.decorator';
+import { Response } from 'express';
+import { CookieService } from 'library/cookie/cookie.service';
+import { TokenService } from 'library/jwt/token.service';
+import { CreateCustomer, LoginCustomer } from 'src/customer/customer.decorator';
 import { CustomerService } from 'src/customer/customer.service';
 
 import { CreateCustomerRequestBodyDto } from 'src/customer/dto/create-customer.dto';
+import { LoginCustomerRequestBodyDto } from 'src/customer/dto/login-customer.dto';
 
 @Controller('customer')
 export class CustomerController {
-  constructor(private readonly customerService: CustomerService) {}
+  constructor(
+    private readonly customerService: CustomerService,
+    private readonly tokenService: TokenService,
+    private readonly cookieService: CookieService,
+  ) {}
 
   @CreateCustomer()
   async createCustomer(
@@ -21,6 +29,22 @@ export class CustomerController {
       password,
       customData,
     });
+    return null;
+  }
+
+  @LoginCustomer()
+  async loginCustomer(
+    @Body() { email, password }: LoginCustomerRequestBodyDto,
+    @Res({ passthrough: true }) response: Response,
+  ) {
+    const result = await this.customerService.validateCustomer({
+      email,
+      password,
+    });
+
+    const token = this.tokenService.generateCustomerToken({ id: result.id });
+    this.cookieService.setCustomerTokenCookie({ response, token });
+
     return null;
   }
 }
