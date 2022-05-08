792b873dffea14569dc1c069db5655fcabff2f6f
argon1025 <argon1025@gmail.com>
2022-05-08 22:47:18 +0900

    feat: 고객정보 수정을 위한 Decorator, Dto, 예외 메시지 구성

--- src/customer/customer.decorator.ts
+++ src/customer/customer.decorator.ts
@@ -1,28 +1,37 @@
-import { applyDecorators, Get, Post } from '@nestjs/common';
-import { ApiOkResponse } from '@nestjs/swagger';
+import { applyDecorators, Get, Patch, Post } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
+import { AdminTokenGuard } from 'library/passport/guard/admin-token.guard';
 import { CustomerTokenGuard } from 'library/passport/guard/customer-token.guard';
 
 import { GetMeCustomerResponseDto } from 'src/customer/dto/get-me-customer.dto';
 
 export const CreateCustomer = () =>
   applyDecorators(
-    Post('auth'),
+    Post('customer/auth'),
     ApiOkResponse({ description: '유저 생성 성공', type: undefined }),
   );
 
 export const LoginCustomer = () =>
   applyDecorators(
-    Post('auth/login'),
+    Post('customer/auth/login'),
     ApiOkResponse({ description: '로그인 성공 및 토큰 발급', type: undefined }),
   );
 
 export const GetMeCustomer = () =>
   applyDecorators(
-    Get('get-me'),
+    Get('customer/get-me'),
     ApiOkResponse({
       description: '고객 정보 로드 성공',
       type: GetMeCustomerResponseDto,
     }),
     CustomerTokenGuard(),
+  );
+
+export const PatchCustomerForStore = () =>
+  applyDecorators(
+    Patch('store/:storeId/customer/:customerId'),
+    ApiOperation({ summary: '관리자 권한으로 고객 정보를 수정합니다' }),
+    ApiOkResponse({ description: '고객 정보 수정 성공', type: undefined }),
+    AdminTokenGuard(),
   );
