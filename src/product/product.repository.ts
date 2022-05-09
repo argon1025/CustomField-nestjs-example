c27902548ee649be71fa714dededf3afe69e2b63
argon1025 <argon1025@gmail.com>
2022-05-09 17:45:43 +0900

    feat: 제품 생성을 위한 Repository

---
+++ src/product/product.repository.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { Product } from '@prisma/client';
+
+import { PrismaClientService } from 'library/prisma/type/prisma.type';
+
+@Injectable()
+export class ProductRepository {
+  create({
+    prismaClientService,
+    id,
+    store,
+    name,
+    price,
+  }: {
+    prismaClientService: PrismaClientService;
+    id: Product['id'];
+    store: Product['store'];
+    name: Product['name'];
+    price: Product['price'];
+  }) {
+    return prismaClientService.product.create({
+      data: { id, store, name, price },
+    });
+  }
+}
